5aaca7925cd3b1fb52f0692112278f59d379c84e
halo-github <82198313@qq.com>
2016-06-21 13:46:47 +0800

    Initial Commit

---
+++ VariBtn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VariBtn
+//
+//  Created by Fenix on 16/6/21.
+//  Copyright © 2016年 Fenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
